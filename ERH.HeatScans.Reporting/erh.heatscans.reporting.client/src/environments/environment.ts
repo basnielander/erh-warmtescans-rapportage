5d152cc7a5abcfa91c8a36bc978827294b7fbef3
Bas Nielander <microsoft@nielander.nl>
2025-12-30 17:21:15 +0100

    minor update

--- ERH.HeatScans.Reporting/erh.heatscans.reporting.client/src/environments/environment.ts
+++ ERH.HeatScans.Reporting/erh.heatscans.reporting.client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://test.nielander.nl/api/', //'https://localhost:7209/api/',
+  apiBaseUrl: 'https://localhost:7209/api/', //'https://test.nielander.nl/api/', //
   googleClientId: '343866397450-jt4c9enoljg2c8n6v65ighfmhsl30cag.apps.googleusercontent.com',
   scopes: [
     'https://www.googleapis.com/auth/drive',
