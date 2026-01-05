a577869637cfc352c6e9230960acafe1538b303a
Bas Nielander <microsoft@nielander.nl>
2026-01-05 12:53:27 +0100

    update deployment

--- ERH.HeatScans.Reporting/erh.heatscans.reporting.client/src/environments/environment.prod.ts
+++ ERH.HeatScans.Reporting/erh.heatscans.reporting.client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:7209/api/',
-  googleClientId: '343866397450-jt4c9enoljg2c8n6v65ighfmhsl30cag.apps.googleusercontent.com',
+  apiBaseUrl: '@@api-base-url@@',
+  googleClientId: '@@google-client-id@@.apps.googleusercontent.com',
   scopes: [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/userinfo.profile'
